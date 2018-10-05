88d7b96faa44d9198eed958f7ab8e7ff980bd911
Dylan Long <dylan.long1115@gmail.com>
2018-10-05 18:38:14 -0500

    forgot to save app shouldve been with last commit

--- src/App.js
+++ src/App.js
@@ -20,21 +20,6 @@ class App extends Component {
   }
 
 
-
-//  searchBarQue = (event) => {
- //   this.setState({searchQue: event.target.value});
-  //  let searchQue = "https://api.themoviedb.org/3/search/multi?api_key=3b444fbb10b743aa99e6a4f27af457d9&language=en-US&query=" + this.state.searchQue + "&page=1&include_adult=false";
-
-  //  axios.get(searchQue)
-  //  .then(response => {
- //     this.setState({results: response.data.results});
- //     console.log(this.state.results);
- //   })
-  
- // }
-
-
-
   render() {
     const movies = this.state.movies.map(movie => {
       return (<Movie title={movie.title} 
@@ -44,16 +29,6 @@ class App extends Component {
        />
        )
     });
-
-  //  const searched = this.state.results.map(movie => {
-   //   return (
-   //     <SearchMovies 
-   //     img={"https://image.tmdb.org/t/p/w300/" + movie.poster_path}
-  //      title={movie.title}
-  //      key={movie.id}
-  //      />
- //      )
- //   });
 
     return (
     <div className="App">
